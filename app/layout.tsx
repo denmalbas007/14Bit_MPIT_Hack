b935bbc16c45e676b9f782ed606fd6f08bf01c27
Vadim Bykov <vadimbykov@MacBook-Air-Vadim.local>
2023-12-05 12:30:50 +0300

    add main page

---
+++ app/layout.tsx
@@ -0,0 +1,71 @@
+import "@/styles/globals.css";
+import { Metadata } from "next";
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/config/fonts";
+import { Providers } from "./providers";
+import { Navbar } from "@/components/navbar";
+import { Link } from "@nextui-org/link";
+import clsx from "clsx";
+import NextLink from "next/link";
+import { Logo } from "@/components/logo";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <div
+            className="relative flex flex-col h-screen bg-cover"
+            style={{ backgroundImage: "url('/images/bg.png')" }}
+          >
+            <NextLink
+              className="flex justify-start items-center gap-1 m-5	"
+              href="/"
+            >
+              <Logo />
+              <img src="logo.svg" alt="TimeBus" />
+            </NextLink>
+            <main className="container mx-auto h-screen flex justify-center items-center">
+              {children}
+            </main>
+            <footer className="w-full flex items-center justify-center py-3">
+              <Link
+                isExternal
+                className="flex items-center gap-1 text-current"
+                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
+                title="nextui.org homepage"
+              ></Link>
+            </footer>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,43 @@
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Avatar,
+  Button,
+} from "@nextui-org/react";
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  weight: "400", // if single weight, otherwise you use array like [400, 500, 700],
+  style: "normal", // if single style, otherwise you use array like ['normal', 'italic']
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-lg text-center justify-center content-center">
+        <Card className="w-[406px] h-[187px] bg-neutral-800 bg-opacity-50 rounded-[16px] border border-white border-opacity-30">
+          <CardBody className="content-center justify-center">
+            <Button color="primary">Войти</Button>
+            <div className={inter.className}>
+              <div className="text-white text-opacity-50 text-base font-medium leading-normal text-center">
+                или
+              </div>
+            </div>
+            <Button variant="bordered">Зарегистрироваться</Button>
+          </CardBody>
+        </Card>
+      </div>
+    </section>
+  );
+}
