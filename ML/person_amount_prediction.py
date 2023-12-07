d53de67d84dbb1b7173ab7c447b950f7826c69c0
denmalbas007 <malbashich@yandex.ru>
2023-12-07 05:58:32 +0300

    update person_amount_prediction.py

--- ML/person_amount_prediction.py
+++ ML/person_amount_prediction.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 
 class Predictor:
-    def __init__(self, model_path, data_path):
+    def __init__(self, model_path):
         self.model = joblib.load(model_path)
-        self.data = pd.read_json(data_path)
 
-    def predict(self) -> float:
-        return int(self.model.predict(self.data))
+    def predict(self, data_json) -> float:
+        data = pd.read_json(data_json)
+        return int(self.model.predict(data))
